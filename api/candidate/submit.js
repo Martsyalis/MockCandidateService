c750664449aed7d20649d9f6bdbcaad1db03cd94
Martsyalis <martsyalis@gmail.com>
2020-03-11 16:58:22 -0700

    promisify submit fn and abstract into module

---
+++ api/candidate/submit.js
@@ -0,0 +1,59 @@
+const AWS = require('aws-sdk'); 
+const uuid = require('uuid');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+async function submit (event) {
+  console.log('got to submit')
+  try {
+    const requestBody = JSON.parse(event.body);
+    const fullname = requestBody.fullname;
+    const email = requestBody.email;
+    const experience = requestBody.experience;
+
+    if (typeof fullname !== 'string' || typeof email !== 'string' || typeof experience !== 'number') {
+      console.error('Validation Failed');
+      return new Error('Couldn\'t submit candidate because of validation errors.')
+    }
+
+    const submitCandidateP = async candidate => {
+      const candidateInfo = {
+        TableName: process.env.CANDIDATE_TABLE,
+        Item: candidate,
+      };
+      await dynamoDb.put(candidateInfo).promise()
+      return candidate
+    };
+    
+    const candidateInfo = (fullname, email, experience) => {
+      const timestamp = new Date().getTime();
+      return {
+        id: uuid.v1(),
+        fullname: fullname,
+        email: email,
+        experience: experience,
+        submittedAt: timestamp,
+        updatedAt: timestamp,
+      };
+    };
+
+    const newCandidate = await submitCandidateP(candidateInfo(fullname, email, experience));
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: `Sucessfully submitted candidate with email ${email}`,
+          candidateId: newCandidate.id
+        })
+    }
+  } 
+  catch(err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: `Unable to submit candidate with email ${email}`
+      })
+    }
+  }
+}
+
+module.exports.submit = submit;
